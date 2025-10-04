6157b02a3f357180ce61abe72e7c3d3ae0d16b71
aaronhtan <aaronhtan@gmail.com>
2025-10-05 00:26:37 +0100

    Claude Code

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -15,6 +15,28 @@ return {
     },
   },
   {
+    'coder/claudecode.nvim',
+    dependencies = { 'folke/snacks.nvim' },
+    Lazy = false,
+    config = function()
+      require('claudecode').setup {
+        terminal_cmd = '/Users/aarontan/.nvm/versions/node/v22.18.0/bin/claude',
+      }
+      -- Set up keymaps after plugin loads
+      local opts = { noremap = true, silent = true }
+      vim.keymap.set('n', '‹leader>ac', '<cmd>ClaudeCode<cr>', vim.tbl_extend('force', opts, { desc = 'Toggle Claude' }))
+      vim.keymap.set('n', '<leader>af', '<cd>ClaudeCodeFocus<cr>', vim.tbl_extend('force', opts, { desc = 'Focus Claude' }))
+      vim.keymap.set('n', '<leader>ar', '<cmd>ClaudeCode --resume<er>', vim.tbl_extend('force', opts, { desc = 'Resume Claude' }))
+      vim.keymap.set('n', '<leader>aC', '<cmd>ClaudeCode --continue<cr>', vim.tbl_extend('force', opts, { desc = 'Continue Claude' }))
+      vim.keymap.set('n', '‹leader>am', '<cmd>ClaudeCodeSelectModel<cr>', vim.tbl_extend('force', opts, { desc = 'Select Claude model' }))
+      vim.keymap.set('n', '<Leader>ab', '<cmd>ClaudeCodeAdd %<cr>', vim.tbl_extend('force', opts, { desc = 'Add current buffer' }))
+      vim.keymap.set('v', '<leader>as', '<cmd>ClaudeCodeSend<cr>', vim.tbl_extend('force', opts, { desc = 'Send to Claude' }))
+      vim.keymap.set('n', '<leader>aa', '<cmd>ClaudeCodeDiffAccept<cr>', vim.tbl_extend('force', opts, { desc = 'Accept diff' }))
+      vim.keymap.set('n', '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>', vim.tbl_extend('force', opts, { desc = 'Deny diff' }))
+    end,
+  },
+
+  {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
